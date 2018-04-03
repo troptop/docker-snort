57fb7b8d109a802574dec6c99318600cd892ae48
root <root@ip-172-31-1-225.eu-west-1.compute.internal>
2018-04-03 08:27:26 +0000

    copy-file.sh change regex

--- copy-file.sh
+++ copy-file.sh
@@ -4,12 +4,16 @@ sourceDir=/opt/log
 DestDir=/var/log/snort
 inotifywait -m $sourceDir -e create |
     while read path action file; do
-	filetmstp=$(echo $file | sed 's/\.[0-9]*//g')
-	fileproc=$(echo $filetmstp | tr '.' '-')
-	kill -9 $(eval echo \$$fileproc)
+        filetmstp=$(echo $file | sed 's/\.[0-9]\{10\}//g')
+        echo $filetmstp
+        fileproc=$(echo $filetmstp | tr '.' '-')
+        echo $fileproc
+        kill -9 $(eval echo \$$fileproc)
         echo "The file '$file' appeared in directory '$path' via '$action'"
         # do something with the file
-	tail -f $path"/"$file > $DestDir"/"$file &
-	eval $fileproc=\$'!'
-	eval echo \$$fileproc		
+        tail -f $path"/"$file > $DestDir"/"$file &
+        eval $fileproc=\$'!'
+        echo "the $fileproc PID process is : "
+        eval echo \$$fileproc
     done
+
